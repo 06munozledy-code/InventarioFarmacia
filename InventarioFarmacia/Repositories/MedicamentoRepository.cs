bc3c036124f8cf9a2ae49b09f84f2247ac1d2d51
Ledy Muñoz Rodríguez <06munozledy@gmail.com>
2026-06-13 10:02:01 -0400

    Closes #4: Implementar interfaz y clase de repositorio para Medicamentos

---
+++ InventarioFarmacia/Repositories/MedicamentoRepository.cs
@@ -0,0 +1,20 @@
+﻿using InventarioFarmacia.Models;
+
+namespace InventarioFarmacia.Repositories
+{
+    public class MedicamentoRepository : IMedicamentoRepository
+    {
+        // conexión a la base de datos 
+        public async Task<IEnumerable<Medicamento>> GetAllAsync()
+        {
+       
+            return await Task.FromResult(new List<Medicamento>());
+        }
+
+        public async Task AddAsync(Medicamento medicamento)
+        {
+            // lógica para guardar en base de datos
+            await Task.CompletedTask;
+        }
+    }
+}
