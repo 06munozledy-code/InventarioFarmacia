02cb09cb376a29e68cd4709c7fa7793b2dbc1cb2
Ledy Muñoz Rodríguez <06munozledy@gmail.com>
2026-06-13 09:33:14 -0400

    Closes #3: Completar implementación de controladores para Laboratorio, Lote y MovimientoInventario

---
+++ InventarioFarmacia/Controllers/MovimientoInventarioController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InventarioFarmacia.Controllers
+{
+    public class MovimientoInventarioController : Controller
+    {
+        public IActionResult Index() => View();
+        public IActionResult Create() => View();
+    }
+}
