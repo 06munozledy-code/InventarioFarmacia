e7b39cdf858f2bf60ddd60dd9c91d77fdb659176
Ledy Muñoz Rodríguez <06munozledy@gmail.com>
2026-06-13 09:18:52 -0400

    Closes #3: Implementar controladores del sistema

---
+++ InventarioFarmacia/Controllers/MedicamentosController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InventarioFarmacia.Controllers
+{
+    public class MedicamentosController : Controller
+
+
+    {
+        // Acción para mostrar la lista de medicamentos
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Acción para crear un nuevo medicamento
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
