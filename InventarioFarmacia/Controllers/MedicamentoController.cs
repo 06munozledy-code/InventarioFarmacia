abbede2c1df06969d53b12086a81688c8348d5f1
Ledy Muñoz Rodríguez <06munozledy@gmail.com>
2026-06-13 11:53:25 -0400

    feat: Implementación completa del MedicamentosController y su vista #2

---
+++ InventarioFarmacia/Controllers/MedicamentoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InventarioFarmacia.Controllers
+{
+    public class MedicamentoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
